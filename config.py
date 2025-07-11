c3df81191462b711b3f9813f4285a9fb15805d9b
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2025-07-12 05:00:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ BOT_TOKEN = "7675990675:AAEQZd4ACNSWshqEnAYdBzl_EuW6FC4KnZY"
 ADMIN_ID = 2076705260
 API_BASE = "https://fluxion-fastapi.onrender.com"
 ADMIN_PASSWORD = "admin"
+DATABASE_URL = "postgresql://fluxionuser:03qjdBfYzY2SJmltMawn9TpUH1mK7F9s@dpg-d1oobrgdl3ps73fmc60g-a.oregon-postgres.render.com/fluxiondb"
