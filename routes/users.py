d3af103bad88d5c0f41844a0dc9cc816563d364f
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2025-07-14 06:20:22 +0700

    Update users.py

--- routes/users.py
+++ routes/users.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import func
 
+from db import SessionLocal
 from database import get_db
 from models.models import User, Poin, Riwayat, Referral, Penarikan, Verifikasi
 from schemas.schemas import StartSessionRequest, ReferralRequest
