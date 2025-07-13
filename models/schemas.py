48c8e88f7b2c0ef22dfee270e104f7f041c37555
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2025-07-13 21:21:59 +0700

    Delete models/schemas.py

--- models/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class StartSessionRequest(BaseModel):
-    user_id: str
-    username: str = "
-
-class ReferralRequest(BaseModel):
-    user_id: str
-    ref_id: str
