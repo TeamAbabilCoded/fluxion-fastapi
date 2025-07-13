4007b1720ab42db876a88e7e779044f64039fa7a
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2025-07-13 18:27:11 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models.models import Base
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 from config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
 
-# Inisialisasi DB (pastikan hanya dipanggil sekali saat startup, atau dari script setup)
-def init_db():
-    Base.metadata.create_all(bind=engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
