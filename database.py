f1bfad63f00dbf3d7a27ebe9fa2a7ebbedf25ba3
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2025-07-13 18:32:50 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
 from config import DATABASE_URL
 
+# Inisialisasi engine dan session
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
+# Dependency untuk mendapatkan session database
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+# Fungsi inisialisasi DB
+def init_db():
+    from models.models import Base  # Import di dalam untuk menghindari circular import
+    Base.metadata.create_all(bind=engine)
