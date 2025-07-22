b6fa73418a6a03fc4a7528e8913f76e2c13ed0b5
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2025-07-22 16:25:42 +0700

    Update models.py

--- models/models.py
+++ models/models.py
@@ -47,13 +47,3 @@ class Penarikan(Base):
     nomor = Column(String)
     time = Column(DateTime)
     status = Column(String, default="pending")
-
-class VoucherGame(Base):
-    __tablename__ = "voucher_game"
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String)
-    game = Column(String)
-    id_game = Column(String)
-    diamond = Column(Integer)
-    status = Column(String, default="pending")
-    time = Column(DateTime, default=datetime.datetime.utcnow)
